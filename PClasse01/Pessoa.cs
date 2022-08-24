a1ee66c771bdf60414ced6cbcb6692b0acefb02d
FelipeCaiko <felipecaiko@gmail.com>
2022-08-24 08:56:46 -0300

    Adicionar arquivos de projeto.

---
+++ PClasse01/Pessoa.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PClasse01
+{
+    internal class Pessoa
+    {
+        String nome;
+        DateTime dtnasc;
+        String cpf;
+        String telefone;
+        public Pessoa()
+        {
+            this.nome = nome;
+            this.dtnasc = dtnasc;
+            this.cpf = cpf;
+            this.telefone = telefone;
+        }
+        public void setNome(String nome)
+        {
+            this.nome = nome;
+        }
+        public void setDtNasc(DateTime dtnasc)
+        {
+            this.dtnasc = dtnasc;
+        }
+        public void setCpf(String cpf)
+        {
+            this.cpf = cpf;
+        }
+        public void setTelefone(String telefone)
+        {
+            this.telefone = telefone;
+        }
+        public String getNome()
+        {
+            return nome;
+        }
+        public DateTime getDtNasc()
+        {
+            return dtnasc;
+        }
+        public String getCpf()
+        {
+            return cpf;
+        }
+        public String getTelefone()
+        {
+            return telefone;
+        }
+        public override string ToString()
+        {
+            return "Nome: " + nome + "\n" +
+                   "Data de Nascimento: " + dtnasc + "\n" +
+                   "CPF: " + cpf + "\n" +
+                   "Telefone: " + telefone + "\n";
+        }
+    }
+}
---
+++ PClasse01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace PClasse01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            Pessoa pessoa2 = new Pessoa();
+            Pessoa pessoa3 = new Pessoa();
+
+            Console.Write("Informe o nome da primeira pessoa: ");
+            pessoa1.setNome(Console.ReadLine());
+
+            Console.Write("Informe a data de nascimento da primeira pessoa: ");
+            pessoa1.setDtNasc(DateTime.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o CPF da primeira pessoa: ");
+            pessoa1.setCpf(Console.ReadLine());
+
+            Console.Write("Informe o telefone da primeira pessoa: ");
+            pessoa1.setTelefone(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.Write("Informe o nome da segunda pessoa: ");
+            pessoa2.setNome(Console.ReadLine());
+
+            Console.Write("Informe a data de nascimento da segunda pessoa: ");
+            pessoa2.setDtNasc(DateTime.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o CPF da segunda pessoa: ");
+            pessoa2.setCpf(Console.ReadLine());
+
+            Console.Write("Informe o telefone da segunda pessoa: ");
+            pessoa2.setTelefone(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.Write("Informe o nome da terceira pessoa: ");
+            pessoa3.setNome(Console.ReadLine());
+
+            Console.Write("Informe a data de nascimento da terceira pessoa: ");
+            pessoa3.setDtNasc(DateTime.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o CPF da terceira pessoa: ");
+            pessoa3.setCpf(Console.ReadLine());
+
+            Console.Write("Informe o telefone da terceira pessoa: ");
+            pessoa3.setTelefone(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine(pessoa1.ToString());
+            Console.WriteLine();
+            Console.WriteLine(pessoa2.ToString());
+            Console.WriteLine();
+            Console.WriteLine(pessoa3.ToString());
+        }
+    }
+}
